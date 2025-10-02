f434f35e49605af0bd54aefe766b1cccce2f1b0a
sadiyabhbb <sadiyabhbb@proton.me>
2025-10-03 02:24:37 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const { Telegraf } = require("telegraf");
+const config = require("./config.json");
+const bot = new Telegraf(config.bot.token);
+require("./main")(bot);
 
+bot.launch().then(() => {
+  console.log(`✅ Bot started as @${config.bot.username}`);
+});
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
